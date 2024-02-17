fb2b5884b5d77358fbc136d8d99216d9f4e4ef17
SatyamSivamPal <satyamsivampal3@gmail.com>
2024-02-17 08:26:08 +0530

    reverse-proxy

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const httpProxy = require('http-proxy')
+
+const app = express();
+const PORT = 8000
+const BASE_PATH = 'https://vercel-clone-satyam.s3.amazonaws.com/__outputs'
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+    const hostName = req.hostname;
+    const subdomain = hostName.split('.')[0];
+
+    const resolveTo = `${BASE_PATH}/${subdomain}`
+    return proxy.web(req, res, {target: resolveTo, changeOrigin: true})
+})
+
+app.listen(PORT, () => console.log(`Reverse Proxy is runnig ... ${PORT}`))
